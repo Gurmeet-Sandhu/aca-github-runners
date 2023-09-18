eabeac6c90f71691c167c66375d9f41bda06878a
Gurmeet Sandhu <gursandhu@deloitte.ca>
2023-09-18 16:44:29 -0700

    fixed workflow

--- github-runner/spin.sh
+++ github-runner/spin.sh
@@ -4,7 +4,6 @@ REPO=$REPO
 PAT=$PAT
 
 RUNNER_NAME="RUNNER-$(hostname)"
-
 TOKEN=$(curl -sX POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${PAT}" https://api.github.com/repos/${GHUSER}/${REPO}/actions/runners/registration-token | jq .token --raw-output)
 cd /home/docker/actions-runner
 ./config.sh --unattended --url https://github.com/${GHUSER}/${REPO} --token ${TOKEN} --name ${RUNNER_NAME} --ephemeral --disableupdate
