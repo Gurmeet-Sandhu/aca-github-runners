c964c0aba753376c335aaa437c20ff9c8d955d36
Gurmeet Sandhu <gursandhu@deloitte.ca>
2024-01-09 14:07:23 -0800

    updated index.js

--- ghr-func-app/ghr-scaler/index.js
+++ ghr-func-app/ghr-scaler/index.js
@@ -22,21 +22,27 @@ module.exports = async function (context, req) {
             context.log(`job ${githubEvent.workflow_job.id} is In-Progress`)
         } else if (githubEvent.action === 'completed') {
             context.log(`job ${githubEvent.workflow_job.id} is Completed`)
-            // Retrieve all messages to find the one corresponding to the completed job
-            let response = await queueClient.receiveMessages({
-                numberOfMessages: 10,
-                visibilityTimeout: 1
-            });
-            if (response.receivedMessageItems.length > 0) {
-                for (let message of response.receivedMessageItems) {
-                    let decodedMsg = Buffer.from(message.messageText, "base64");
-                    context.log(`the message text is ${decodedMsg}`);
-                    if (decodedMsg.includes(`Job ${githubEvent.workflow_job.id}`)) {
-                        // Delete the specific message corresponding to the completed job
-                        await queueClient.deleteMessage(message.messageId, message.popReceipt);
-                        break;
+
+            let msgDeleted = false
+            while(msgDeleted == false){
+                // Retrieve 10 messages to find the one corresponding to the completed job
+                let response = await queueClient.receiveMessages({
+                    numberOfMessages: 10,
+                    visibilityTimeout: 1
+                });
+                if (response.receivedMessageItems.length > 0) {
+                    for (let message of response.receivedMessageItems) {
+                        let decodedMsg = Buffer.from(message.messageText, "base64");
+                        context.log(`the message text is ${decodedMsg}`);
+                        if (decodedMsg.includes(`Job ${githubEvent.workflow_job.id}`)) {
+                            // Delete the specific message corresponding to the completed job
+                            await queueClient.deleteMessage(message.messageId, message.popReceipt);
+                            msgDeleted = true
+                            break;
+                        }
                     }
                 }
+
             }
         }
 
@@ -50,8 +56,4 @@ module.exports = async function (context, req) {
         };
     }
 
-    // const name = (req.query.name || (req.body && req.body.name));
-    // const responseMessage = name
-    //     ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-    //     : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 }
